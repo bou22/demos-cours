1c17ff30e3f928fcc6d5bc1e4c8df00af7f172ab
Claude Boutet <claude.boutet@cegepat.qc.ca>
2021-03-29 19:01:47 -0400

    Code de l'exercice sur le calendrier dynamique en JavaScript.

---
+++ javascript/calendrier/js/dates-fr-en.js
@@ -0,0 +1,7 @@
+/**
+ * Mots français pour les jours et mois
+ */
+
+const mois = ['Janvier', 'Février', 'Mars','Avril','Mai','Juin','Juillet','Août','Septembre','Octobre','Novembre','Décembre'];
+
+const jours = ['D','L','M','M','J','V','S'];
