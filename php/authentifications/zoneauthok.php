b8aae45579f730cadbcd2bf8de9081f922c78fdb
Claude Boutet <claude.boutet@cegepat.qc.ca>
2021-04-06 17:52:17 -0400

    Logique d'authentification incluant des redirections et sans la gestion de la session.

--- php/authentifications/zoneauthok.php
+++ php/authentifications/zoneauthok.php
@@ -5,6 +5,12 @@
         <title>Authentification</title>
     </head>
     <body>
+    <?php 
+        /**
+         * Cette page n'est pas protégée des accès sans authentification.
+         * La prochaine étape inclue une gestion de la session.
+         */
+    ?>
         <p>La zone est accessible seulement une fois authentifié.</p>
     </body>
 </html>
