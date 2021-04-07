cb508dbd8a26cf28ccdd4a0ae3ed292b765ed39b
Claude Boutet <claude.boutet@cegepat.qc.ca>
2021-04-07 13:01:59 -0400

    Logique validation ok et redirection avec header.

---
+++ php/validationForms/zoneauthok.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Authentification</title>
+    </head>
+    <body>
+    <?php 
+        /**
+         * Cette page n'est pas protégée des accès sans authentification.
+         * La prochaine étape sera de mettre en place une 
+         * gestion de la session d'utilisation.
+         */
+    ?>
+        <p>La zone est accessible seulement une fois authentifié.</p>
+    </body>
+</html>
