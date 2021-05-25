8a0bed0c8b780cf0dbfe8d43e93081db49180950
bou22 <33974616+bou22@users.noreply.github.com>
2021-05-25 08:57:53 -0400

    Fonction mail() de PHP

--- php/mail/index.php
+++ php/mail/index.php
@@ -10,8 +10,20 @@ and open the template in the editor.
         <title></title>
     </head>
     <body>
-        <?php
-            
-        ?>
+<?php
+echo envoyerMail("8197600499@msg.telus.com", "Allo !");
+
+function envoyerMail($to, $message) {
+     $subject = '1er mail avec php';
+     $headers = 'From: 8197600499@msg.telus.com' . "\r\n" .
+     'Reply-To: 8197600499@msg.telus.com' . "\r\n" .
+     'X-Mailer: PHP/' . phpversion();
+
+     return mail($to, $subject, $message, $headers);    
+}
+
+
+
+?>
     </body>
 </html>
