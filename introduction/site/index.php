16afbd157c8ac18d627e6468345130796e708fed
bou22 <33974616+bou22@users.noreply.github.com>
2022-02-02 10:03:00 -0500

    Notions sur les tableau HTML et les boucles FOR en PHP. Le code inclut le tableau qui affiche une partie de UTF-8; de U+0000 è U+FFFF.

---
+++ introduction/site/index.php
@@ -0,0 +1,69 @@
+<html>
+    <head>
+        <title>Une page web</title>
+        <style>
+            table {
+                width: 50%;
+                border-collapse: collapse;
+                border: 1px solid black;
+            }
+        </style>
+    </head>
+    <body>
+        <p>Allo ! <a href="tableau.php">Tableau</a>
+            <b>toi !</b>
+            <?php
+                echo ' Tu viens de La Sarre ?';
+                
+            ?>
+            <script type="text/javascript"> 
+                window.document.write("  Ou du Temiscamingue ?")
+            </script>
+            <?php
+                echo ' Peut-être de Guigues ?';
+            ?>
+            </p>
+
+            <table>
+            <?php
+                $debut = mb_ord("A", "UTF-8");
+                $fin = mb_ord("Z", "UTF-8");
+                $ligne = $debut;
+                $cellule = $debut;
+
+                while ($cellule <= $fin)
+                {
+                    $position = $cellule-$ligne; //différence max de 5.
+
+                    switch ($position)
+                    {
+                        case 0:
+                            echo "<tr><td>". chr($cellule)."<td>";
+
+                            if($cellule==$fin && $position < 4) //dernière ligne non complète
+                            {
+                                $fin = $fin+4-$position;
+                            }
+                            $cellule++;
+                            break;
+                        case 4:
+                            echo "<td>".chr($cellule)."<td><tr>";
+
+                            $cellule++;
+                            $ligne = $ligne+5;
+                            break;
+                        default:
+                            echo "<td>".chr($cellule)."<td>";
+
+                            if($cellule==$fin && $position < 4) //dernière ligne non complète
+                            {
+                                $fin = $fin+4-$position;
+                            }
+                            $cellule++;                         
+                    }
+                }
+            ?>      
+            </table>  
+        
+    </body>
+</html>
---
+++ introduction/site/tableau.php
@@ -0,0 +1,52 @@
+<?php
+set_time_limit(0);
+?>
+<html>
+    <head>
+    <meta charset="utf-8">
+    <title>Création d'un tableau dynamique</title>
+    <style>
+        table {
+            width: 50%;
+            border-collapse: collapse;
+        }
+        table tr td {
+            border: 1px solid black;
+            background-color: #ddd;
+        }
+
+    </style>
+    </head>
+    <body>
+        <h1>Caractères unicode</h1>
+        <h2><a href="https://unicode-table.com/en/blocks/unified-canadian-aboriginal-syllabics/">Tableau de caractères unicode</a></h2>
+        <h2><a href="https://fr.wikipedia.org/wiki/Unicode#UTF.2C_Universal_Transformation_Format">Tableau de caractères unicode</a></h2>
+        <p>Ce tableau créé dynamiquement contient une liste des caractères unicode.</p>
+        <p>Ce n'est pas une représentation fidèle des caractères contenu dans UTF-8. Le tableau représente les caractères des points de code de 0000 à 7FFFFFFF.</p>
+        <table>
+            <tr><th>Hexadécimales</th><th>Caractères</th><th>Hexadécimales</th><th>Caractères</th></tr>
+        <?php
+
+            $debut = hexdec("0");
+            $finA = hexdec("F");
+            $finBCD = hexdec("FFFF");
+
+            for ($d=0; $d < $finBCD; $d++)
+            {
+                echo "<tr>";
+                echo "<td>U+".strtoupper(dechex($debut))."</td><td>".mb_chr($debut++,'UTF-8')."</td>";
+                if ($d<$finBCD)
+                {
+                    $d++;
+                    echo "<td>U+".strtoupper(dechex($debut))."</td><td>". mb_chr($debut++,'UTF-8')."</td>";
+                }else 
+                {
+                    echo "<td>&nbsp;</td><td>&nbsp;</td>";
+                }
+                
+                echo "</tr>";
+            }
+        ?>            
+        </table>
+    </body>
+</html>
