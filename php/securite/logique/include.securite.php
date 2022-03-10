373810e919499233fcc2be97ff4a6294940080bf
bou22 <33974616+bou22@users.noreply.github.com>
2022-03-10 14:12:25 -0500

    Champs validés avec messages dans la GUi, mais les valeurs initiales ne sont pas réinscrites.

--- php/securite/logique/include.securite.php
+++ php/securite/logique/include.securite.php
@@ -1,6 +1,65 @@
 <?php
-// Deuxième exercice: afficher le résultat de la validation dans le formulaire.
+//  Exercice: utiliser les bons filtres de nettoyages et/ou de validations.
+// Le résultat des validations doit être affiché dans resultat-validation.php
 
-setcookie("courriel","guy@lafleur.ca");
 
-echo $_COOKIE['courriel'];
+session_start();
+
+// Cette logique est pour fin de démonstration et repose sur une exigence que 
+// tous les champs du formulaire soient valides et obligatoires.
+
+$donneesRequeteOk = true; //Sera changé en false si une des données est invalide.
+
+if (filter_input(INPUT_GET, "entier",FILTER_VALIDATE_INT)){
+    $_SESSION['entier'] = filter_input(INPUT_GET, "entier",FILTER_SANITIZE_NUMBER_INT);
+} else {
+    setcookie('entier',"0", time()+30,"/");
+    $donneesRequeteOk = false;
+}
+
+
+if (filter_input(INPUT_GET, "float", FILTER_VALIDATE_FLOAT)){
+    $_SESSION['float'] = filter_input(INPUT_GET, "float",FILTER_SANITIZE_NUMBER_FLOAT,FILTER_FLAG_ALLOW_FRACTION);
+} else {
+    setcookie('float',"0", time()+30,"/");
+    $donneesRequeteOk = false;
+}
+
+
+if (filter_input(INPUT_GET, "courriel", FILTER_VALIDATE_EMAIL)){
+    $_SESSION['courriel'] = filter_input(INPUT_GET, "courriel",FILTER_SANITIZE_EMAIL);
+} else {
+    setcookie('courriel',"0", time()+30,"/");
+    $donneesRequeteOk = false;
+}
+
+if (filter_input(INPUT_GET, "url", FILTER_VALIDATE_URL)){
+    $_SESSION['url'] = filter_input(INPUT_GET, "url",FILTER_SANITIZE_URL);
+} else {
+    setcookie('url',"0", time()+30,"/");
+    $donneesRequeteOk = false;
+}
+
+if (!empty($_GET['user'])){
+    $_SESSION['user'] = filter_input(INPUT_GET, "user",FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+} else {
+    setcookie('user',"0", time()+30,"/");
+    $donneesRequeteOk = false;
+}
+
+if (!empty($_GET['mdp'])){
+    $_SESSION['mdp'] = filter_input(INPUT_GET, "mdp",FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+} else {
+    setcookie('mdp',"0", time()+30,"/");
+    $donneesRequeteOk = false;
+}
+
+switch ($donneesRequeteOk) {
+    case true:
+        header("Location: ../resultat-validation.php"); //tout est ok continuer
+        break;
+    
+    default:
+    header("Location: ../validation-formulaire.php"); //Problème recommencer
+        break;
+}
--- php/securite/validation-formulaire.php
+++ php/securite/validation-formulaire.php
@@ -1,5 +1,19 @@
 <?php
     include_once 'include.config.php';
+
+    //Automatiser la lecture sera efficace.
+    $erreursRecues = array();
+    error_log("COOKIE: ".isset($_COOKIE));
+    if (isset($_COOKIE)){
+        foreach ($_COOKIE as $key => $value) {
+            //La valeur du cookie est déterminé; aucune autre valeur que 0 est acceptable.
+            if ($value === "0"){
+                $erreursRecues[$key] = 0; //Chaque cookie est ajouté
+                setcookie($key,"",0,"/"); //Le cookie est détruit
+            }
+        }
+    }
+    session_start();
 ?>
 <html>
     <head>
@@ -12,9 +26,9 @@
         }
         label {
             display: block;
-            width: 40%;
+            width: 100%;
             margin: 0px auto 0 auto;
-            text-align: right;
+            text-align: left;
             font-size: 1em;
         }
         label input {
@@ -29,40 +43,69 @@
             border: 3px solid #000;
             padding: 2px 2px 2px 10px;
         }
+        span {
+            font-size: 0.8em;
+            color: #000;
+            background-color: yellowgreen;
+        }
     </style>
         <title>Programmation serveur avec PHP</title>
     </head>
     <body>
         <h1>Protection de l'application par la validation des requêtes du client</h1>
         <form method="get" action="./logique/include.securite.php" name="formDonnees" id="formDonnees">
-            <label for="entier">Un nombre entier
+            <label for="entier">Un nombre entier</label>
                 <input type="text" name="entier" id="entier">
-            </label>
-            <label for="float">Un nombre float
+            
+                <?php
+                    if (isset($erreursRecues["entier"]) && $erreursRecues["entier"]===0) {
+                        echo "<span>Utiliser un nombre entier uniquement.</span>";
+                    }
+                ?>
+            <label for="float">Un nombre float</label>
                 <input type="text" name="float" id="float">
-            </label>
-            <label for="courriel">Un courriel
+            
+            <?php
+                    if (isset($erreursRecues["float"]) && $erreursRecues["float"]===0) echo "<span>Utiliser un nombre à point flottant avec un '.'.</span>";
+             ?>
+
+            <label for="courriel">Un courriel</label>
                 <input type="text" name="courriel" id="courriel">
-            </label>
-            <label for="url">Un url
-                <input type="text" name="url" id="url">
-            </label>
-            <label for="user">Un nom d'utilisateur
+            
+            <?php
+                    if (isset($erreursRecues["courriel"]) && $erreursRecues["courriel"]===0) echo "<span>Utiliser un adresse courriel en format valide.</span>";
+            ?>
+            <label for="url">Un url</label>
+                <input type="text" name="url" id="url" 
+                <?php if (isset($_SESSION["url"])) echo "value=".$_SESSION["url"]; ?>
+                >
+            
+            <?php
+                    if (isset($erreursRecues["url"]) && $erreursRecues["url"]===0) {
+                        echo "<span>Utiliser un URL au format valide.</span>";
+                    } 
+                ?>
+            <label for="user">Un nom d'utilisateur</label>
                 <input type="text" name="user" id="user">
-            </label>
-            <label for="mdp">Un mot de passe
+            
+            <?php
+                    if ( (isset($erreursRecues["user"]) && $erreursRecues["user"]===0) || (isset($erreursRecues["mdp"]) && $erreursRecues["mdp"]===0) ) 
+                        echo "<span>Votre nom d'usager et/ou mot de passe contient un/des caractère(s) non-valide(s).</span>";
+                ?>
+            <label for="mdp">Un mot de passe</label>
                 <input type="password" name="mdp" id="mdp" >
-            </label>
-            <label>
+            
+
+            <label></label>
             <input type="button" onclick="formDonnees.submit()" value="Vérifier les valeurs inscrites">
-            </label>
+            
         </form>
     </body>
     <script type="text/javascript">
         let champsTextes =Array.from(document.getElementsByTagName('input'));
 
         champsTextes.forEach(function(champ) {
-            champ.setAttribute("size","40");
+            champ.setAttribute("size","10");
         });
     </script>
 </html>
