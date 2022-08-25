3aa58cda4dff9403b9899a5449aff52f4e8bb95c
bou22 <bou22@hotmail.com>
2022-08-25 16:12:15 -0400

    Avec le routage sur GET, sur POST et sur donnée embarquée dans l'url.

--- src/nodejs/nodejs-demo-intro/src/server.mjs
+++ src/nodejs/nodejs-demo-intro/src/server.mjs
@@ -1,5 +1,5 @@
 /** Importation des modules de couches intermédiaires: middleware */
-import express from "express";
+import express, { urlencoded } from "express";
 import { getRandom } from "./modules/random.mjs";
 import path from "path";
 
@@ -10,7 +10,8 @@ const HOST = '0.0.0.0';
 /**
  * Le frameword express comme serveur web.
  */
-const app = express();
+const app = express()
+app.use(express.urlencoded({ extended: false}))
 
 /** Routage de l'url racine. Les documents dans le répertoire sont rendus en réponse. */
 app.get('/', (req, res) => {
@@ -25,8 +26,24 @@ app.get('/info', (req, res) => {
 
 /** Routage de l'url /echo. Le serveur récupère le paramètre get. */
 app.get('/echo', (req, res) => {
-  let v = req.param('info');
+  let v = req.query.info
   res.send('Le résultat: ' + v);
+});
+
+/** Routage de l'url /majuscule. Le serveur récupère le paramètre get. */
+app.get('/majuscule/:mot1/:mot2', (req, res) => {
+  let info1 = req.params.mot1
+  let info2 = req.params.mot2
+  let info3 = req.query.info
+  res.send('En majuscule SVP: ' + info1.toUpperCase() + info2.toUpperCase() + info3);
+});
+
+/** Routage de l'url avec la méthode POST.  */
+app.post('/post', (req, res) => {
+  /**req.body est une structure de donnée prête à être utilisée. */
+  let info = req.body 
+  console.log(info)
+  res.send('Le champ de la requête post : ' + info.genre);
 });
 
 /** Démarrage du serveur */
