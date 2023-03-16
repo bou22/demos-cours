82504fc0f568b4912fb23b9a1afade99b009d517
claude.boutet <claude.boutet@cegepat.qc.ca>
2023-03-16 14:58:57 -0400

    Démo d'authentification en cours.

--- src/php/authentifications/acces/authentification.php
+++ src/php/authentifications/acces/authentification.php
@@ -1,4 +1,8 @@
 <?php
+/**
+ * Démonstration de la mécanique de permission/refus d'accès à une page à l'aide d'une session.
+ */
+
 //Authentification très très simplifiée
 //Le nom d'usager et le mot de passe sont bons.
 if(false){
--- src/php/authentifications/auth.redirect.php
+++ src/php/authentifications/auth.redirect.php
@@ -1,12 +1,22 @@
 <?php
+            /**
+             * La validation ne sera effectuée seulement si une valeur de nom d'usager est reçue par la requête http.
+             */
 if (!empty($_POST['usr'])){
     $usr = filter_input(INPUT_POST, "usr", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
     $pwd = filter_input(INPUT_POST, "pwd", FILTER_DEFAULT); //Pour le mot de passe, les caractères spéciaux sont recommandés
     
+            /**
+             * La vérification if($usr === "Joe" && $pwd === "allo") de la conformité des informations fournies 
+             * est hardcodée pour cet exemple. La validation de $usr et de $pwd serait comparé normalement avec des données
+             * lues sur une base de données.
+             */
     if($usr === "Joe" && $pwd === "allo"){
+        //La correspondance des informations est fausse.
+
         header("Location: index.php?erreur=user");
     } else{
-        //Aucunes erreurs la session peut être créée
+        //Aucunes erreurs ou mauvaise information, la session peut être créée
 
         ini_set("session.cookie_lifetime", 0);
         ini_set("session.use_cookies", 1);
@@ -21,26 +31,24 @@ if (!empty($_POST['usr'])){
         ini_set("session.hash_function" , "sha512");
 
         define("SESSIONNAME", hash_hmac($user));
+        session_name(SESSIONNAME);
 
         if (session_status() == PHP_SESSION_NONE) {
-            session_name(SESSIONNAME);
+            session_name($user);
             session_start();
             $_SESSION['authentification'] = TRUE;
             $_SESSION['user'] = $usr;
-        } else {
-            session_destroy();
-            $_SESSION = array();
+            $_SESSION['sessionname'] = SESSIONNAME;
 
-            session_name(SESSIONNAME);
-            session_start();
-            $_SESSION['authentification'] = TRUE;
-            $_SESSION['user'] = $usr;
+            error_log(" L'usager: ".$user." s'est authentifié.",3, "/home/claude/logs/acces-application.log");
+        } else {
+                // La session existe déjà, mais ne devrait pas; ça mérite d'être journalisé
+            error_log(" L'usager: ".$user." La session existait lors de l'authentification.",3, "/home/claude/logs/acces-application.log");
         }
-        //Voir comment gérer les timestamp de session
+            //Voir comment gérer les timestamp de session
         header("Location: zoneprivee.php?b=".SESSIONNAME);
     }
 } else {
     header("Location: index.php?erreur=vide");
 }
-
 
