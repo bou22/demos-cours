80b4c0555f59cced2fefc0ab016eec4f16d7961f
bou22 <bou22@hotmail.com>
2024-02-20 08:13:29 -0500

    Structure HTML du document mise en place et un css pour identifier les éléments.

---
+++ src/php/html-css/articles-abonnement/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Abonnement nécesaire pour lire plus de 3 articles</title>
+</head>
+<body>
+    <section id="principale">
+        <div id="gauche">Articles récents</div>
+        <div id="droit">article</div>
+    </section>
+    <section id="pied">Liens du pied de page</section>
+</body>
+</html>
